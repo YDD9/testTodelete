fab0dd811cebb41b25085367c99c83c0cac2f6f4
LOGON\212534368 <shuai.hu@ge.com>
2017-10-07 18:16:36 +0200

    better structured

--- setup.py
+++ setup.py
@@ -3,10 +3,17 @@
 from distutils.core import setup
 
 setup(name='myUniqueAnalytics',
-      version='1.0',
+      version='1.2dev',
+      packages=['analytics', 'analytics.test'],
+      scripts=['bin/excel2Json.py','bin/json2Excel.py'],
       description='Python setup test',
+      license='LICENSE.txt',
       author='YDD9',
       author_email='YDD9@unknown.org',
       url='https://github.com/YDD9/testTodelete.git/',
-      packages=['analytics'],
+      long_description=open('README.mk').read(),
+	  install_requires=[
+        "Django >= 1.1.1",
+        "caldav == 0.1.4",
+      ],
      )
